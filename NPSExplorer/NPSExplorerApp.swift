1ff4f49f16dba429bfde35525288728cb5a249ad
Stefan Cutovic <scuto26@uw.edu>
2025-03-08 23:19:00 -0800

    Initial Commit

---
+++ NPSExplorer/NPSExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPSExplorerApp.swift
+//  NPSExplorer
+//
+//  Created by Stefan Cutovic on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NPSExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
